61c98c671980d7dcde56ac7b2bd0e70f3897896f
scarecrowcxc <scarecrow_cxc@hotmail.com>
2019-01-08 21:01:18 +0800

    change

--- flask-demo/app/__init__.py
+++ flask-demo/app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # 据说这两步就好比django中的主子分发那种感觉
     from .app_1 import app_1 as app_1_blueprint
     app.register_blueprint(app_1_blueprint, url_prefix = '/app_1')
 
