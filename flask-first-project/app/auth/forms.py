3409412d9398d510a5f7961e82349d563a2be64c
scarecrowcxc <scarecrow_cxc@hotmail.com>
2019-01-10 22:40:08 +0800

    8d finished

--- flask-first-project/app/auth/forms.py
+++ flask-first-project/app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email, regexp, EqualTo
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from ..models import User
 
@@ -16,9 +16,9 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     username = StringField('Username', validators=[Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, ''number, dots or underscores')])
-    password = PasswordField('Password', validators=[Required(), EqualTo('Password2', message='Password must match.')])
+    password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Password must match.')])
     password2 = PasswordField('Confirm password', validators=[Required()])
-    submit = submitField('Register')
+    submit = SubmitField('Register')
 
 
     def validate_email(self, field):
--- flask-first-project/app/auth/views.py
+++ flask-first-project/app/auth/views.py
@@ -1,9 +1,14 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required,\
+        current_user
 from .. models import User
 from .forms import LoginForm
 # 这里从当前文件夹下导入了auth 蓝图吧？
 from . import auth
+from .. import db
+from ..email import send_email
+from .forms import LoginForm, RegistrationForm
+
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -26,5 +31,15 @@ def logout():
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
 
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        flash('You canm now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
 
