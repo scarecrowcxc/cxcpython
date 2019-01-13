ba2d34e4fd8cb6e0a0df3d821a0f11d6c8e421b8
scarecrowcxc <scarecrow_cxc@hotmail.com>
2019-01-13 17:17:57 +0800

    8h finished

--- flask-first-project/tests/test_user_model.py
+++ flask-first-project/tests/test_user_model.py
@@ -28,7 +28,7 @@ class UserModelTestCase(unittest.TestCase):
     def test_password_verification(self):
         u = User(password='cat')
         self.assertTrue(u.verify_password('cat'))
-        self.assertFalse(u.verify_password('cat'))
+        self.assertFalse(u.verify_password('dog'))
 
 
     def test_password_salts_are_random(self):
